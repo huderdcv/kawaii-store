2ae7961fee54d3bde6d18bfd45630c18ba2a850c
huderdcv <huder.delacruz.27@unsch.edu.pe>
2025-07-10 17:19:22 -0500

    fix: solucionar error de interface theme-category duplicado

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,5 @@
 export * from './address.interface';
-export * from './theme-category.interface';
+// export * from './theme-category.interface';
 export * from './country.interface';
 export * from './paypal.interface';
 export * from './product.interface';
