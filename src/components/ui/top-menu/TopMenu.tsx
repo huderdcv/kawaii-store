f64bedd068cdbfcadd4715b493cf3b25997a06b8
huderdcv <huder.delacruz.27@unsch.edu.pe>
2025-07-10 18:36:40 -0500

    style: cambiar diseño del header

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 
 import Link from 'next/link';
 import { IoSearchOutline, IoCartOutline } from 'react-icons/io5';
+import { Menu, X } from 'lucide-react'; // Íconos
 
 import { titleFont } from '@/config/fonts';
 import { useCartStore, useUIStore } from '@/store';
@@ -44,7 +45,7 @@ export const TopMenu = () => {
   };
 
   return (
-    <nav className="flex px-5 justify-between items-center w-full">
+    <nav className="flex px-5 justify-between items-center w-full bg-[#FF7C00]">
       {/* Logo */}
       <div>
         <Link href="/">
@@ -58,77 +59,83 @@ export const TopMenu = () => {
       {/* Center Menu */}
       <div className="hidden sm:block">
         <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          className="m-2 p-2 rounded-md transition-all hover:bg-[#ffaf63]"
           href="/franchise/pokemon"
         >
           Pokemon
         </Link>
         <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          className="m-2 p-2 rounded-md transition-all hover:bg-[#ffaf63]"
           href="/franchise/dragonball"
         >
           Dragon Ball
         </Link>
         <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          className="m-2 p-2 rounded-md transition-all hover:bg-[#ffaf63]"
           href="/franchise/bts"
         >
           BTS
         </Link>
         <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          className="m-2 p-2 rounded-md transition-all hover:bg-[#ffaf63]"
           href="/franchise/none"
         >
           Sin franquicia
         </Link>
       </div>
 
-      {/* desplegable */}
-      {/* Pantallas pequeñas */}
-      <div className="sm:hidden relative" ref={dropdownRef}>
-        <button
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          className="p-2 m-2 border rounded-md w-full text-left bg-white shadow-sm"
-        >
-          Franquicias
-        </button>
-
-        {isDropdownOpen && (
-          <div className="absolute z-10 w-full bg-white shadow-md rounded-md mt-2">
-            <Link
-              onClick={handleLinkClick}
-              className="block px-4 py-2 hover:bg-gray-100"
-              href="/franchise/pokemon"
-            >
-              Pokemon
-            </Link>
-            <Link
-              onClick={handleLinkClick}
-              className="block px-4 py-2 hover:bg-gray-100"
-              href="/franchise/dragonball"
-            >
-              Dragon Ball
-            </Link>
-            <Link
-              onClick={handleLinkClick}
-              className="block px-4 py-2 hover:bg-gray-100"
-              href="/franchise/bts"
-            >
-              BTS
-            </Link>
-            <Link
-              onClick={handleLinkClick}
-              className="block px-4 py-2 hover:bg-gray-100"
-              href="/franchise/none"
-            >
-              Sin franquicia
-            </Link>
-          </div>
-        )}
-      </div>
-
       {/* Search, Cart, Menu */}
       <div className="flex items-center">
+        {/* desplegable */}
+        {/* Pantallas pequeñas */}
+        <div className="sm:hidden relative" ref={dropdownRef}>
+          <button
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            className={`p-2 m-2 border-none rounded-md shadow-sm flex items-center gap-2 ${
+              !isDropdownOpen ? 'bg-[#FF7C00]' : 'bg-[#ffaf63]'
+            } hover:bg-[#ffaf63]`}
+          >
+            {isDropdownOpen ? (
+              <X className="w-5 h-5" />
+            ) : (
+              <Menu className="w-5 h-5" />
+            )}
+            <span>Franquicias</span>
+          </button>
+
+          {isDropdownOpen && (
+            <div className="absolute z-10 w-full bg-white shadow-md rounded-md mt-2">
+              <Link
+                onClick={handleLinkClick}
+                className="block px-4 py-2 hover:bg-[#ffaf63]"
+                href="/franchise/pokemon"
+              >
+                Pokemon
+              </Link>
+              <Link
+                onClick={handleLinkClick}
+                className="block px-4 py-2 hover:bg-[#ffaf63]"
+                href="/franchise/dragonball"
+              >
+                Dragon Ball
+              </Link>
+              <Link
+                onClick={handleLinkClick}
+                className="block px-4 py-2 hover:bg-[#ffaf63]"
+                href="/franchise/bts"
+              >
+                BTS
+              </Link>
+              <Link
+                onClick={handleLinkClick}
+                className="block px-4 py-2 hover:bg-[#ffaf63]"
+                href="/franchise/none"
+              >
+                Sin franquicia
+              </Link>
+            </div>
+          )}
+        </div>
         <Link href="/search" className="mx-2">
           <IoSearchOutline className="w-5 h-5" />
         </Link>
@@ -149,7 +156,7 @@ export const TopMenu = () => {
 
         <button
           onClick={openSideMenu}
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          className="m-2 p-2 rounded-md transition-all hover:bg-[#ffaf63]"
         >
           Menú
         </button>
